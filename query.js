59d52b9954b703fe302cf09045ed612634a63472
Marco Schmalz <marco@schess.ch>
2019-02-28 16:57:17 +0100

    Example queries

--- query.js
+++ query.js
@@ -1,8 +1,31 @@
 async function main() {
   const knex = require('./knex/knex.js');
+  let result
 
-  var result = await knex.from('user')
-  console.log(result)
+  result = await knex('question')
+                 .select('question', 'answer')
+                 .where('quiz_id', 1)
+                 .orderBy('order')
+                 .join('answer', {'question.id': 'answer.question_id'})
+                 .where('is_correct', true)
+  for (let line of result) {
+    console.log(line.question, line.answer)
+  }
+
+  result = await knex('quiz')
+                .select('name')
+                .count('question.id as count')
+                .join('question', {'quiz.id': 'question.quiz_id'})
+                .groupBy('quiz.id')
+                .orderBy('count', 'desc')
+
+
+  for (let line of result) {
+    console.log(`${line.name} (${line.count} Fragen)`)
+  }
+
+
+
   knex.destroy()
 }
 
