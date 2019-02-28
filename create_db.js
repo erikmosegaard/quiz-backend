5399dd94b9bea7d142ae45c4ab6d9a3225c13cad
Marco Schmalz <marco@schess.ch>
2019-02-28 14:39:59 +0100

    db schema

--- create_db.js
+++ create_db.js
@@ -7,6 +7,28 @@ async function main() {
     table.timestamp('created_at')
   })
 
+  await knex.schema.createTable('question', table => {
+    table.increments('id')
+    table.text('question')
+    table.integer('order')
+    table.integer('quiz_id').notNullable()
+    table.foreign('quiz_id').references('quiz.id')
+  })
+
+  await knex.schema.createTable('answer', table => {
+    table.increments('id')
+    table.text('answer')
+    table.boolean('is_correct')
+    table.integer('question_id').notNullable()
+    table.foreign('question_id').references('question.id')
+  })
+
+  await knex.schema.createTable('quiz', table => {
+    table.increments('id')
+    table.text('name')
+    table.integer('creator_id')
+    table.foreign('creator_id').references('user.id')
+  })
   knex.destroy()
 }
 
