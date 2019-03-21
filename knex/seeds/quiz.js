80f8862ace1becb47821630075548820715597ed
Marco Schmalz <marco@schess.ch>
2019-03-21 13:58:47 +0100

    Unified seed file

--- knex/seeds/quiz.js
+++
@@ -1,9 +0,0 @@
-
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('quiz').del()
-  await knex('quiz').insert([
-    {id: 1, name: 'JS Quiz'},
-    {id: 2, name: 'Geographie-Quiz'},
-  ]);
-};
